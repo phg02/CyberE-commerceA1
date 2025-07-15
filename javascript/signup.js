f2e6d147c0432c7f15e02792562d327b95688755
phg02 <s4063236@rmit.edu.vn>
2025-07-15 14:41:51 +0700

    new pages

--- javascript/signup.js
+++ javascript/signup.js
@@ -28,3 +28,88 @@ eyebtn1.addEventListener("click", function () {
     eyebtn1.classList.add("ri-eye-off-line");
   }
 });
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    //documents
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+    const confirmPassword = document.querySelector('#confirm-password');
+
+    //regrex
+    const lowerCaseLetters = /[a-z]/g;
+    const upperCaseLetters = /[A-Z]/;
+    const numbers = /[0-9]/;
+    const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+
+    let error = false;
+    const regrex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    let errorList = '';
+
+    if (email.value === '' || email.value == null) {
+        error = true;
+        errorList += '<li>Email is required</li>';
+
+    }
+    if (regrex.test(email.value) === false) {
+        error = true;
+        errorList += '<li>Email is invalid</li>';
+    }
+    if (password.value === '' || password.value == null) {
+        error = true;
+        errorList += '<li>Please enter a password</li>';
+
+    }
+    if (password.value.length < 8) {
+        error = true;
+        errorList += '<li>Password must be at least 8 character</li>';
+
+    }
+    if (!lowerCaseLetters.test(password.value)) {
+        error = true;
+        errorList += '<li>Password must contain a lowercase letter</li>';
+
+    }
+
+    if (!upperCaseLetters.test(password.value)) {
+        error = true;
+        errorList += '<li>Password must contain a uppercase letter</li>';
+
+    }
+    if (!numbers.test(password.value)) {
+        error = true;
+        errorList += '<li>Password must contain a number letter</li>';
+    }
+
+    if (!specialCharacters.test(password.value)) {
+        error = true;
+        errorList += '<li>Password must contain a special character</li>';
+
+    }
+    if (password.value !== confirmPassword.value) {
+        error = true;
+        errorList += '<li>Password not match</li>';
+    }
+    if (grecaptcha.getResponse().length <= 0) {
+        error = true;
+        errorList += '<li>Please complete the captcha</li>';
+    }
+
+
+    let errors = "<ul class='error-list'>" + errorList + "</ul>";
+    if (error == true) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            html: errors
+        });
+        return
+    }
+    form.submit();
+})
